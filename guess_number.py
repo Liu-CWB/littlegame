eefb294bf19e382f6650c8580ac6f0e2b6aef294
Liu-CWB <34766410+Liu-CWB@users.noreply.github.com>
2018-11-07 11:41:37 +0800

    Update guess_number.py

--- guess_number.py
+++ guess_number.py
@@ -1,15 +1,15 @@
-import random
-llimit, rlimit = 1, 99
-number = random.randint(llimit, rlimit)
-guess = -1
-while guess != number:
-    guess = int(input('請猜%i至%i間其中1個整數，不包含%i及%i'%(llimit, rlimit, llimit, rlimit)))
-    while guess <= llimit or guess >= rlimit:
+import random   #載入隨機亂數套件
+llimit, rlimit = 1, 99   #設定區間
+number = random.randint(llimit, rlimit)     #讓電腦從區間內隨機挑選1個整數作為終極密碼
+guess = -1    #為了讓while迴圈一定可以執行而給定的初始猜測值
+while guess != number:   #迴圈，當猜測值不等於終極密碼時才執行
+    guess = int(input('請猜%i至%i間其中1個整數，不包含%i及%i'%(llimit, rlimit, llimit, rlimit)))    #請輸入者輸入整數(開始猜測)
+    while guess <= llimit or guess >= rlimit:   #為避免輸入者輸入不合理的整數而寫的迴圈
         guess = int(input('不要鬧了，請猜%i至%i間其中1個整數，不包含%i及%i'%(llimit, rlimit, llimit, rlimit)))
-        if guess > number:
+        if guess > number:      #調整區間
         llimit = llimit
         rlimit = guess
-    elif guess < number:
+    elif guess < number:        #調整區間
         llimit = guess
         rlimit = rlimit
 print('遊戲結束，因為終極密碼為%i'%number)
