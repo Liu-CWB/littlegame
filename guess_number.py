52e6765a97d5deff88dfccaa8e58b490b29624cd
Liu-CWB <34766410+Liu-CWB@users.noreply.github.com>
2018-11-07 11:42:48 +0800

    Update guess_number.py

--- guess_number.py
+++ guess_number.py
@@ -6,7 +6,7 @@ while guess != number:   #迴圈，當猜測值不等於終極密碼時才執行
     guess = int(input('請猜%i至%i間其中1個整數，不包含%i及%i'%(llimit, rlimit, llimit, rlimit)))    #請輸入者輸入整數(開始猜測)
     while guess <= llimit or guess >= rlimit:   #為避免輸入者輸入不合理的整數而寫的迴圈
         guess = int(input('不要鬧了，請猜%i至%i間其中1個整數，不包含%i及%i'%(llimit, rlimit, llimit, rlimit)))
-        if guess > number:      #調整區間
+    if guess > number:      #調整區間
         llimit = llimit
         rlimit = guess
     elif guess < number:        #調整區間
